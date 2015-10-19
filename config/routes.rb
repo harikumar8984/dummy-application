590b5dc045d009c44d6c4b3541a57ce9f905e0db
harikumar <hari.kumar@ruby-software.com>
2015-10-19 16:24:04 +0530

    reverting back the changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,11 @@ Nuryl::Application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  # namespace :api, defaults: { format: :json },
-  #           constraints: { subdomain: 'api' }, path: '/'  do
-  #   scope module: :v1 , path: '/v1',
-  #         constraints: ApiConstraints.new(version: 1, default: true) do
-  #     # We are going to list our resources here
-  #     devise_for :users
-  #   end
-  # end
-
-  namespace :api do
-    namespace :v1  do
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1 , path: '/v1',
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
       devise_for :users
     end
   end
