16eaae28e951822d209f3f61acc0d5b7bf73fe13
harikumar <hari.kumar@ruby-software.com>
2015-10-19 16:14:34 +0530

    test for routes in production

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,17 @@ Nuryl::Application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  namespace :api, defaults: { format: :json },
-            constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1 , path: '/v1',
-          constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
+  # namespace :api, defaults: { format: :json },
+  #           constraints: { subdomain: 'api' }, path: '/'  do
+  #   scope module: :v1 , path: '/v1',
+  #         constraints: ApiConstraints.new(version: 1, default: true) do
+  #     # We are going to list our resources here
+  #     devise_for :users
+  #   end
+  # end
+
+  namespace :api do
+    namespace :v1  do
       devise_for :users
     end
   end
