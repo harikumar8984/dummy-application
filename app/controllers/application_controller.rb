45dfddc920c73b9e7e6b08276d164567691afdb7
harikumar <hari.kumar@ruby-software.com>
2015-10-21 13:14:47 +0530

    filters for authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_filter :authenticate_device
+  before_filter :authenticate_device
   # This is our new function that comes before Devise's one
-  #before_filter :authenticate_user_from_token!
+  before_filter :authenticate_user_from_token!
   # # This is Devise's authentication
   # before_filter :authenticate_user!
 
