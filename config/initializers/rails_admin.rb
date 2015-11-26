d9ad164017035fc276ea7b7bcf2ee046a8efb301
harikumar <hari.kumar@ruby-software.com>
2015-11-26 10:55:39 +0530

    current user not getting in rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -13,7 +13,9 @@ RailsAdmin.config do |config|
 
 
   config.authorize_with do
-    redirect_to main_app.root_path unless current_user.admin?
+    if current_user
+        redirect_to main_app.root_path unless current_user.admin?
+    end
   end
 
   config.current_user_method(&:current_user)
