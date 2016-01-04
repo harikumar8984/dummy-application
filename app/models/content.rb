672c9f107a295e5438a79f104f266cb3082b781c
harikumar <hari.kumar@ruby-software.com>
2016-01-04 16:05:04 +0530

    MP3 metadata editor

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,6 @@
 require 'openssl'
 require 'base64'
+require 'mp3info'
 
 class Content < ActiveRecord::Base
   has_many :course_contents , dependent: :destroy
@@ -10,6 +11,7 @@ class Content < ActiveRecord::Base
   validates :status, inclusion: { in: %w(ACTIVE INACTIVE) , message: "%{value} is not a valid status" }
   validates :name, :status, :content_type, presence: true
   mount_uploader :name, ContentUploader
+  before_save :save_meta_data_content
   scope :active, -> { where(status: 'ACTIVE') }
 
   def content_type_enum
@@ -22,6 +24,27 @@ class Content < ActiveRecord::Base
 
   def is_file_exist?
     self.name.file.exists? || File.exists?(self.name.path)
+  end
+
+  def save_meta_data_content
+    info = Mp3Info.open(self.name.path)
+    unless info.tag.nil?
+      self.title = info.tag.album if self.title.blank?
+      self.artist =  info.tag.artist if self.artist.blank?
+    end
+    unless info.tag2.nil?
+      self.creator = info.tag2.TCOM  if self.creator.blank?
+    end
+
+    Mp3Info.open(self.name.path) do |mp3|
+      unless info.tag.nil?
+        mp3.tag.album = self.title unless self.title.blank?
+        mp3.tag.artist = self.artist unless self.artist.blank?
+      end
+      unless info.tag2.nil?
+        mp3.tag2.TCOM = self.creator unless self.creator.blank?
+      end
+    end
   end
 
 end
