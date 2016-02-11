c58e23dea74ad7d00ff41d440030864ff8ff98f6
harikumar <hari.kumar@ruby-software.com>
2016-02-11 11:59:03 +0530

    test for fixes of search box for content association

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -63,43 +63,43 @@ RailsAdmin.config do |config|
       field :l_name
     end
   end
-
-  config.model Content do
-    configure :duration do
-      read_only true
-    end
-    list do
-      field :id
-      field :name do
-        formatted_value do # used in form views
-          value.to_s.upcase
-        end
-
-        pretty_value do # used in list view columns and show views, defaults to formatted_value for non-association fields
-          value.titleize
-        end
-
-        export_value do
-          value.camelize # used in exports, where no html/data is allowed
-        end
-      end
-      field :status
-    end
-    include_all_fields
-    exclude_fields :created_at, :updated_at, :course_contents, :courses, :progress, :player_usage_stats
-  end
-
-  config.model 'Course' do
-    object_label_method do
-      :custom_label_method
-    end
-    include_all_fields
-    field :contents do
-      searchable :name
-    end
-
-    exclude_fields :created_at, :updated_at, :course_contents, :progress
-  end
+  #
+  # config.model Content do
+  #   configure :duration do
+  #     read_only true
+  #   end
+  #   list do
+  #     field :id
+  #     field :name do
+  #       formatted_value do # used in form views
+  #         value.to_s.upcase
+  #       end
+  #
+  #       pretty_value do # used in list view columns and show views, defaults to formatted_value for non-association fields
+  #         value.titleize
+  #       end
+  #
+  #       export_value do
+  #         value.camelize # used in exports, where no html/data is allowed
+  #       end
+  #     end
+  #     field :status
+  #   end
+  #   include_all_fields
+  #   exclude_fields :created_at, :updated_at, :course_contents, :courses, :progress, :player_usage_stats
+  # end
+  #
+  # config.model 'Course' do
+  #   object_label_method do
+  #     :custom_label_method
+  #   end
+  #   include_all_fields
+  #   field :contents do
+  #     searchable :name
+  #   end
+  #
+  #   exclude_fields :created_at, :updated_at, :course_contents, :progress
+  # end
 
   RailsAdmin::ApplicationController.class_eval do
     before_filter :authenticate_user!
