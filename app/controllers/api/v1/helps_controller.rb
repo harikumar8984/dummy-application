de7f94ac7d203daf98dbe308e090ed6465cdb5b2
harikumar <hari.kumar@ruby-software.com>
2016-03-17 11:18:16 +0530

    remove binding

--- app/controllers/api/v1/helps_controller.rb
+++ app/controllers/api/v1/helps_controller.rb
@@ -7,11 +7,8 @@ class  Api::V1::HelpsController < ApplicationController
   respond_to :json
 
   def create
-    binding.pry
    help_obj =  Help.create(help_params.merge(status: 'new'))
-   binding.pry
     begin
-      binding.pry
       @client.post_tickets(help_desk_params(help_obj.id))
     rescue Exception => ex
       return render :status => 404, :json => {:success => false, data: "Fresh Desk error: #{ex.message}"}
