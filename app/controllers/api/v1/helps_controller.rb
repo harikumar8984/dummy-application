42d84e6f8e33ca6401eedb17692852bef7e37f67
harikumar <hari.kumar@ruby-software.com>
2016-04-19 22:11:19 +0530

    remove freshdesk api

--- app/controllers/api/v1/helps_controller.rb
+++ app/controllers/api/v1/helps_controller.rb
@@ -3,16 +3,16 @@ class  Api::V1::HelpsController < ApplicationController
   skip_before_filter :authenticate_scope!, :only => [:create, :help_desk_webhook]
   skip_before_filter :authenticate_user_from_token!, :only =>  [:create, :help_desk_webhook]
   skip_before_filter :authenticate_device, :only => [:create, :help_desk_webhook]
-  before_filter :fresh_desk_intialize, :only => [:create]
+  #before_filter :fresh_desk_intialize, :only => [:create]
   respond_to :json
 
   def create
    help_obj =  Help.create(help_params.merge(status: 'new'))
-    begin
-      @client.post_tickets(help_desk_params(help_obj.id))
-    rescue Exception => ex
-      return render :status => 404, :json => {:success => false, data: "Fresh Desk error: #{ex.message}"}
-    end
+    # begin
+    #   @client.post_tickets(help_desk_params(help_obj.id))
+    # rescue Exception => ex
+    #   return render :status => 404, :json => {:success => false, data: "Fresh Desk error: #{ex.message}"}
+    # end
     UserMailer.help_mail(help_params).deliver
     return render :status => 201, :json => {:success => true}
   end
@@ -22,13 +22,13 @@ class  Api::V1::HelpsController < ApplicationController
     params.permit(:name, :email, :description)
   end
 
-  def help_desk_params(id)
-    help_params.merge(subject: "Support Needed For #{params[:name]}_##{id}" )
-  end
+  # def help_desk_params(id)
+  #   help_params.merge(subject: "Support Needed For #{params[:name]}_##{id}" )
+  # end
 
-  def fresh_desk_intialize
-  @client = Freshdesk.new( ENV['FreshDesk_Url'],  ENV['FreshDesk_Api_Key'])
-  end
+  # def fresh_desk_intialize
+  # @client = Freshdesk.new( ENV['FreshDesk_Url'],  ENV['FreshDesk_Api_Key'])
+  # end
 
   # def help_desk_webhook
   #   return render :status => 201, :json => {:success => true}
