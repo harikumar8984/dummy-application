d82edcc03e64d58f78eb90a9e84e084e95df45ff
harikumar <hari.kumar@ruby-software.com>
2016-05-05 16:59:56 +0530

    forcing https in payment form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,7 @@ class  TransactionsController < ApplicationController
   skip_before_filter :authenticate_user_from_token!, :only => [:new, :get_stripe_plan]
   skip_before_filter :authenticate_device, :only => [:new, :get_stripe_plan]
   before_filter :authenticate_user!
-
+  force_ssl if: :ssl_configured?
   respond_to :json
 
   def new
@@ -16,19 +16,25 @@ class  TransactionsController < ApplicationController
 
   def get_stripe_plan
     all_plan = StripeExt.get_all_plan
-    @plan = []
-    all_plan[:data].each do |plan|
-      amount = plan.amount > 0 ? (plan.amount.to_f/100) : 0.00
-      if params[:user_type] == 'beta' && plan.id != 'Yearly'
-        @plan << [(plan.id == 'Beta' ? 'Yearly' : plan.id.to_s) + " ($"+ amount.to_s+")", plan.id]
-      elsif plan.id != 'Beta' && params[:user_type] != 'beta'
-        if params[:user_type] == 'gift'
-          @plan << [plan.id.to_s + " ($"+ amount.to_s+")", plan.id] if plan.id == 'Yearly'
-        else
-          @plan << [plan.id.to_s + " ($"+ amount.to_s+")", plan.id]
+    if all_plan
+      @plan = []
+      all_plan[:data].each do |plan|
+        amount = plan.amount > 0 ? (plan.amount.to_f/100) : 0.00
+        if params[:user_type] == 'beta' && plan.id != 'Yearly'
+          @plan << [(plan.id == 'Beta' ? 'Yearly' : plan.id.to_s) + " ($"+ amount.to_s+")", plan.id]
+        elsif plan.id != 'Beta' && params[:user_type] != 'beta'
+          if params[:user_type] == 'gift'
+            @plan << [plan.id.to_s + " ($"+ amount.to_s+")", plan.id] if plan.id == 'Yearly'
+          else
+            @plan << [plan.id.to_s + " ($"+ amount.to_s+")", plan.id]
+          end
         end
       end
     end
+  end
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 
 end
