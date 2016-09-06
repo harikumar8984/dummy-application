488d95272a90648ed7220e0bb549bc322589abcf
Rahul <rahulchhlotre@gmail.com>
2016-09-06 23:24:10 +0530

    change config ssl setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  #config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -73,7 +73,7 @@ Rails.application.configure do
       :authentication => :plain,
       :user_name      => ENV["SENDGRID_USERNAME"],
       :password       => ENV["SENDGRID_PASSWORD"],
-      :domain         => 'heroku.com'
+      :domain         => 'https://staging-nuryl.herokuapp.com'
   }
 
 
